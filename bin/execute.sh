6ab3ed8ce6d781e11f2f27efd08a4cc112fb44a0
Mateus Furquim <furquim.axs@gmail.com>
2019-03-19 15:40:51 -0300

    Ready to build and deploy on labbs

--- bin/execute.sh
+++ bin/execute.sh
@@ -1,8 +1,3 @@
-docker pull mfurquim/metrics-generator:1.0.0
-docker pull flaviostutz/grafana:5.2.4
-docker pull prom/prometheus:v2.7.2
-
-cp -vR dashboards/ grafana/provisioning/dashboards
 cp -vR rules/*2.7.yml prometheus/rules/
 cp -vR rules/over_average_of_peaks.yml prometheus/rules/
 
@@ -10,4 +5,4 @@ docker-compose build
 
 docker-compose up -d
 
-./bin/simulate.sh &
+#./bin/simulate.sh &
